640120247dc6063b06252ea67cf349dbe3194db1
Grant Hesketh <ghesketh76@gmail.com>
2020-11-03 19:56:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   ["bob","chris","joe","jane"]
-  .unshift ("arrays!")
+  .unshift ("wow")
 end
 
 def remove_element_from_end_of_array(array)
