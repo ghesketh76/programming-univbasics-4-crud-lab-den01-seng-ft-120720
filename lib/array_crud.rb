da86fec521fc7e4045c527e664035efe2d8c89c9
Grant Hesketh <ghesketh76@gmail.com>
2020-11-03 20:05:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,8 +19,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  ["1","2","3","arrays!","4"]
-  
+  ["1","2","3","arrays!"]
+  .pop
   
 end
 
